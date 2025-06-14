c97321a89852acc3b25421078e0db454631f11a9
lucashmsilva <lucashenrique580@gmail.com>
2025-06-14 13:09:47 -0300

    chore: updates the app setup to use the new features

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,61 +1,70 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"sync/atomic"
+	"time"
 
 	"github.com/bermr/api-golang-base/internal/config"
 	"github.com/bermr/api-golang-base/internal/infra/server"
+	"github.com/bermr/api-golang-base/internal/middlewares"
+	"github.com/bermr/api-golang-base/internal/tools/logger"
 	"github.com/go-chi/chi/v5"
 )
 
 /*
-	TODO: validate unit tests for the logging module
 	TODO: extract the logging module as its own lib
+	TODO: implement unit tests for the logging module
 	TODO: learn and implement dependency injection
 	TODO: setup the logger as an injected dependency
 */
 
 func main() {
 	var isShuttingDown atomic.Bool
+	var loggerOutputStream io.Writer
+
+	var loggerMdw *middlewares.RequestLoggerMiddleware
+	var errorMdw *middlewares.ErrorMiddleware
+
 	config, err := config.LoadConfig()
 	if err != nil {
 		slog.Error(fmt.Sprintf("Error loading config: %v", err))
 		return
 	}
+
+	loggerOutputStream = logger.OutputStream(config)
+	slog.SetDefault(logger.GetLogger(config, loggerOutputStream).GetBaseLogger())
+
 	router := chi.NewRouter()
+
+	loggerMdw = middlewares.NewLoggerMiddleware(config, loggerOutputStream)
+	errorMdw = middlewares.NewErrorMiddleware()
+
+	// scopes a log context for the current request
+	router.Use(loggerMdw.HandleRequest)
+
+	// catch-all for in-request panics
+	router.Use(errorMdw.HandleRequest)
+
+	router.Handle("GET /healthcheck", healthcheckHandler())
 
 	srv := server.New(config, router)
 	go srv.Start()
 
-	slog.Info("Config loaded successfully")
-
-	maxBatchSize := 1
-	firehoseLogStream, err := gn_logger.NewFirehoseLogStream(gn_logger.FirehoseLogStreamOptions{
-		StreamName:   "test_logs",
-		MaxBatchSize: &maxBatchSize,
-	})
-	if err != nil {
-		slog.Info("firehose stream creation error", "err", err)
-	}
-
-	logger, err := gn_logger.NewLogger(gn_logger.LoggerOptions{
-		AppName: config.AppName,
-		Version: "1.0.1",
-		Level:   "info",
-		Output:  firehoseLogStream,
-	})
-	if err != nil {
-		slog.Info("logger creation error", "err", err)
-	}
-
-	logger.Log(context.TODO(), "info", "hello world of logs")
-	logger.Log(context.TODO(), "info", "just sent my first log")
-	logger.Log(context.TODO(), "info", "did you get it?")
-
 	srv.GracefulShutdown(&isShuttingDown)
-	firehoseLogStream.Close()
+
+	if outStreamCloser, ok := loggerOutputStream.(io.Closer); ok {
+		outStreamCloser.Close()
+	}
+}
+
+func healthcheckHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 2)
+		// panic(errors.New("hi"))
+		fmt.Fprintln(w, "OK")
+	})
 }
